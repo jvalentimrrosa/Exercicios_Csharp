de28804103c7a9d1631219ef8eb9cf268608075a
José Valentim das Neves Rosa <jvalentim@tecgraf.puc-rio.br>
2019-11-26 16:10:28 -0300

    v1_26NOV

---
+++ Sem_Polimorfismo/Sem_Polimorfismo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player audio1 = new Player();
+            audio1.playCD();
+            Player audio2 = new Player();
+            audio2.playMP3();
+            Console.Read();
+         
+        }
+    }
+}
